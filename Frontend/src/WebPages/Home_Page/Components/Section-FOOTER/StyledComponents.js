b007f9c19db2ff55b4816912109a341eeffa82ab
MuffinMan <58001294+CezarMarcu@users.noreply.github.com>
2023-02-18 00:01:14 +0200

    Finish Footer section

--- Frontend/src/WebPages/Home_Page/Components/Section-FOOTER/StyledComponents.js
+++ Frontend/src/WebPages/Home_Page/Components/Section-FOOTER/StyledComponents.js
@@ -1,16 +1,23 @@
 import styled from "styled-components";
-import {FacebookOutline} from "@styled-icons/evaicons-outline"
+import {Facebook} from '@styled-icons/entypo-social/Facebook'
+import {Instagram} from '@styled-icons/icomoon/Instagram'
+import {Youtube} from '@styled-icons/bootstrap/Youtube'
+import {GooglePlusSquare} from '@styled-icons/fa-brands/GooglePlusSquare'
+import {Phone} from '@styled-icons/boxicons-solid/Phone'
+import {CreditCard2BackFill} from '@styled-icons/bootstrap/CreditCard2BackFill'
+import {ShieldFillCheck} from '@styled-icons/bootstrap/ShieldFillCheck'
+
 export const Container = styled.div`
     width:100%;
     position:relative;
-    height:40rem;
+    height:auto;
     background:rgb(229,232,235);
     top:258.58rem;
 `
 export const Content = styled.div`
     position:relative;
     width:66%;
-    height:100%;
+    height:auto;
     background:transparent;
     margin:0 auto;
     display:flex;
@@ -53,4 +60,140 @@ export const NewsLetterContainer = styled.div`
     width:25rem;
     background:transparent;
     flex-direction:column; 
+`
+export const Text = styled.p`
+    font-family:'Roboto slab';
+    margin-top:0rem;
+    margin-left:1rem;
+    width:24rem;
+    text-align: left;
+`
+export const Icons = styled.div`
+    height:auto;
+    width:10rem;
+    background:transparent;
+    flex-direction:row;
+    display:flex;
+    gap:0.5rem;
+    margin-left:1rem;
+`
+export const FacebookIcon = styled(Facebook)`
+    width:3rem;
+    height:3rem;
+    color:gray;
+`
+export const InstagramIcon = styled(Instagram)`
+    width:3rem;
+    height:3rem;
+    color:gray;
+`
+export const YoutubeIcon = styled(Youtube)`
+    width:3rem;
+    height:3rem;
+    color:gray;
+`
+export const GoogleIcon = styled(GooglePlusSquare)`
+    width:3rem;
+    height:3rem;
+    color:gray;
+`
+export const PhoneIcon = styled(Phone)`
+    width:5rem;
+    height:5rem;
+    color:gray;
+    margin-left:-16rem;
+`
+export const CardIcon = styled(CreditCard2BackFill)`
+    width:5rem;
+    height:5rem;
+    color:gray;
+    margin-left:-16rem;
+`
+export const ShieldIcon = styled(ShieldFillCheck)`
+    width:5rem;
+    height:5rem;
+    color:gray;
+`
+export const PaymentOptionContainer = styled.div`
+    weight:auto;
+    height:auto;
+    display:flex;
+    padding-left:20rem;
+    padding-top:7rem;
+`
+export const SecuredShoppingContainer = styled.div`
+    weight:auto;
+    height:auto;
+    display:flex;
+    padding-left:20rem;
+    padding-top:7rem;
+`
+
+export const InputValue = styled.p`
+    display:flex;
+    font-family:'Roboto Slab';
+    font-weight:900;
+    margin-top:2rem;
+    transition:0.5s;
+`
+export const InputForm = styled.input`
+    width:100%;
+    height:0%;
+    background:transparent;
+    border-top:none;
+    border-left:none;
+    border-right:none;
+    border-bottom:none;
+    transition:1s;
+    :focus{
+        outline:none;
+    }
+`
+export const Input = styled.div`
+    width:27rem;
+    height:4rem;
+    display:flex;
+    flex-direction:column;
+    background:transparent;
+    border-top:none;
+    border-left:none;
+    border-right:none;
+    border-bottom:1px solid rgb(190,190,190);
+    :hover{
+        ${InputValue}{
+            margin-top:0;
+            font-size:13px;
+            color:rgb(37,199,58);
+        }
+
+        ${InputForm}{
+            height:50%;
+        }
+        border-bottom:2px solid rgb(37,199,58);
+    }
+`
+
+export const Subscribe = styled.button`
+    height:3rem;
+    width:10rem;
+    margin:0 auto;
+    margin-top:2rem;
+    float:right;
+    font-family:'Roboto Slab';
+    font-weight:900;
+    font-size:16px;
+`
+export const Subtitle = styled.p`
+    font-size:19px;
+    font-family:'Roboto Slab';
+    font-weight:900;
+    display:flex;
+    margin-left:20px;
+`
+
+export const AsistenceContainer = styled.div`
+    width:20rem;
+    height:auto;
+    margin-top:3rem;
+    background:transparent;
 `
--- Frontend/src/WebPages/Home_Page/Components/Section-FOOTER/index.js
+++ Frontend/src/WebPages/Home_Page/Components/Section-FOOTER/index.js
@@ -1,7 +1,35 @@
-import { Container,Content, LinksContainer, Title,Link, LFRContainer, NewsLetterContainer,FacebookIcon } from "./StyledComponents"
+import { useState } from "react"
+import { Container,
+    Content,
+    LinksContainer,
+    Title,Link, 
+    LFRContainer,
+    NewsLetterContainer,
+    FacebookIcon,
+    InstagramIcon,
+    YoutubeIcon,
+    GoogleIcon,
+    Icons,
+    Text,
+    Input,
+    InputValue,
+    InputForm,
+    Subscribe,
+    AsistenceContainer,
+    Subtitle,
+    PhoneIcon,
+    CardIcon,
+    ShieldIcon,
+    PaymentOptionContainer,
+    SecuredShoppingContainer} from "./StyledComponents"
+
 const Footer = () => {
+    const [isClicked, setClick] = useState(false)
+    const onClickHandler = () => isClicked===true ? setClick(false) : setClick(true);
+
     return(
         <Container>
+            
             <Content>
                 <LinksContainer>
                     <Title>Linkuri utile</Title>
@@ -14,15 +42,66 @@ const Footer = () => {
                     <Link>POLITICA DE CONFIDENTIALITATE</Link>
                     <Link>POLITICA DE RETUR</Link>
                 </LinksContainer>
+                
                 <LFRContainer>
                     <Title>Livrare gratuita</Title>
+                    <Text>Pentru comenzi peste 150 de lei daca toate produsele pot fi livrate intr-un singur colet</Text>
                     <Title>Follow Us</Title>
+                    <Icons>
+                        <FacebookIcon></FacebookIcon>
+                        <InstagramIcon></InstagramIcon>    
+                        <YoutubeIcon></YoutubeIcon>
+                        <GoogleIcon></GoogleIcon>
+                    </Icons>         
                     <Title>Recomandam</Title>
                 </LFRContainer>
+
                 <NewsLetterContainer>
                     <Title>Aboneaza-te la newsletter</Title>
+                    <Text>Primeste promotii,noutati si oferte personalizate direct in inbox</Text>
+                    
+                    <Input onClick={onClickHandler}>
+                        <InputValue>Adresa e-mail*</InputValue>
+                        <InputForm/>
+                    </Input>
+
+                     <Input onClick={onClickHandler}>
+                        <InputValue>Nume</InputValue>
+                        <InputForm/>
+                    </Input>
+
+                     <Input onClick={onClickHandler}>
+                        <InputValue>Prenume</InputValue>
+                        <InputForm/>
+                    </Input>
+
+                    <Subscribe>Aboneaza-te</Subscribe>
+                    
+                    
                 </NewsLetterContainer>
             </Content>
+
+            <Content>
+                <AsistenceContainer>
+                    <PhoneIcon></PhoneIcon>
+                    <Subtitle>Asistenta telefonica</Subtitle>
+                    <Text>Suport comenzi si oferta online</Text>
+                    <Text>Luni-Vineri: 9:00-20:00</Text>
+                    <Text>04221421</Text>
+                </AsistenceContainer>
+
+                <PaymentOptionContainer>
+                    <CardIcon></CardIcon>
+                    <Subtitle>Optiuni de plata</Subtitle>
+                </PaymentOptionContainer>
+
+                <SecuredShoppingContainer>
+                    <ShieldIcon></ShieldIcon>
+                    <Subtitle>Shoping securizat</Subtitle>
+                </SecuredShoppingContainer>
+                
+            </Content>
+
         </Container>
     )
 }
